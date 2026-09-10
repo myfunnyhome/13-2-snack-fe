f98546600712607fe85a3fbe7768c688e166e93f
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-09-10 18:57:37 +0900

    feat: add size prop

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/utils/cn';
 type ButtonProps = {
   text: string;
   type?: 'button' | 'submit';
+  size?: 'sm' | 'md' | 'lg';
   disabled?: boolean;
   onClick?: () => void;
   variant?: 'primary' | 'secondary';
@@ -14,10 +15,16 @@ const buttonVariant = {
   primary: 'bg-primary-950 text-white',
   secondary: 'bg-white text-primary-950 border border-primary-300',
 };
+const buttonSize = {
+  sm: 'h-[40px]',
+  md: 'h-[44px]',
+  lg: 'h-[64px]',
+};
 
 export default function Button({
   text,
   type = 'button',
+  size = 'lg',
   disabled = false,
   onClick,
   variant = 'primary',
@@ -31,7 +38,8 @@ export default function Button({
       className={cn(
         'w-full h-[64px] rounded-[2px] flex items-center justify-center text-16-bold',
         buttonVariant[variant],
-        disabled && 'bg-primary-100 text-primary-300',
+        buttonSize[size],
+        disabled && 'bg-primary-100 text-primary-300 border-0',
         className,
       )}
     >
