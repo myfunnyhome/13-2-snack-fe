a1c978f2cbf408cd5ba9859c9f1a78c3753190c9
lucy <lucy.email@example.com>
2026-09-22 19:49:29 +0900

    feat: ReactQueryDevtools 추가

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -3,12 +3,14 @@
 import { type PropsWithChildren, useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function QueryProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       {children}
     </QueryClientProvider>
   );
